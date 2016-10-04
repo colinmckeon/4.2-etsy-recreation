e65fafa8a1423de1b2e99290b309dd647f5377d0
Colin McKeon <14mckeon@gmail.com>
2016-10-04 16:32:55 -0400

    JSONP set up and functioning

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,19 +1,24 @@
+// //
+// // alert ("colin");
 //
-// alert ("colin");
+//
+// var $ = require('jquery');
+// var handlebars = require("handlebars");
+//
+//
+// var source = $('#id').html();
+// var template = handlebars.compile(source);
+//
+//
+//
+// //use .append instead of html to continue stamping, otherwise .html will replace the first
+// $('#id').html(template());
+// $('#id').append(template());
+//
+//
 
 
-var $ = require('jquery');
-var handlebars = require("handlebars");
 
-
-var source = $('#id').html();
-var template = handlebars.compile(source);
-
-
-
-//use .append instead of html to continue stamping, otherwise .html will replace the first
-$('#id').html(template());
-$('#id').append(template());
 
 /************************************************
 JSON
@@ -28,3 +33,39 @@ fetchJSONP(url, displayProducts);
 /************************************************
 JSON ^^^^^^^^^
 ************************************************/
+
+
+
+/*
+  (url: String, callback: Function) -> undefined
+
+  Execute a callback function with the JSON results from the url specified.
+
+  Examples
+      var url = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=yarn&includes=Images,Shop";
+
+      fetchJSONP(url, function(data) {
+        // do something with data
+      });
+
+      // OR
+
+      function logData(data) {
+        console.log(data);
+      }
+
+      fetchJSONP(url, logData);
+*/
+function fetchJSONP(url, callback) {
+    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+    var script = document.createElement('script');
+
+    window[callbackName] = function(data) {
+        delete window[callbackName];
+        document.body.removeChild(script);
+        callback(data);
+    };
+
+    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+    document.body.appendChild(script);
+}
